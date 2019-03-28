edf1b3ec462f2d62e0b91b69af4260ee478bb881
Alex Peteniov <@alex.p>
2019-03-28 18:37:46 +0200

    Разбил на два модуля проект (PropertyModule, ProductModule), по одному на вкладку с их кмопонентами

--- src/app/products/propertyModule/property.model.ts
+++ src/app/products/propertyModule/property.model.ts
@@ -1,6 +1,6 @@
 export class Property {public name: string; public type: string;
 constructor(name: string, type: string) {
     this.name = name;
-    this.type = type;/
+    this.type = type;
   }
 }
