c829c9b893959fd0482bbbaa8cef894578a63834
Alex Peteniov <@alex.p>
2019-04-03 05:47:02 +0300

    Reactive Form is done. Adding FormControls. Generate ui components.

--- src/app/products/productModule/product-edit/product-edit.component.ts
+++ src/app/products/productModule/product-edit/product-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -11,17 +12,21 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class ProductEditComponent implements OnInit {
   product: Product;
-  productService: ProductService;
   pForm: FormGroup;
-  constructor() {}
+  constructor(
+    private productService: ProductService,
+    private toastr: ToastrService) {}
+
   ngOnInit() {
     this.pForm = new FormGroup({
       name: new FormControl('', Validators.required),
-      userEmail: new FormControl('', Validators.required),
-      userPhone: new FormControl('', Validators.required)});
+      price: new FormControl('', Validators.required),
+      dateAdd: new FormControl('', Validators.required)});
   }
-  onSubmit(form: FormGroup) {
+  onSubmit(form: FormGroup = this.pForm) {
     const value = form.value;
     const newProduct = new Product(value.name, value.price, value.dateAdd);
     this.productService.addProduct(newProduct);
-  }}
+    this.toastr.success('Продукт добавлено успешно!', 'Toastr message!', {positionClass: 'toast-top-center'});
+  }
+  }
